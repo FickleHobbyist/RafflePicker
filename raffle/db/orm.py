74cdfc002e0a47851cbbf80661fe1ca99f7199ac
FickleHobbyist <alexnmay@gmail.com>
2020-11-01 22:11:24 -0800

    - Added name length limit to user names across the board - In User, renamed user_sales to sales - Added date_started to Drawing, with default - Also changed Dates in the drawing table to DateTimes - Simplified the display of User and Drawing to avoid long lists of Sale and Winner objects

--- raffle/db/orm.py
+++ raffle/db/orm.py
@@ -12,12 +12,15 @@ engine = create_engine('sqlite:///:memory:', echo=True)
 Session.configure(bind=engine)
 Base = declarative_base()
 
+name_length_limit = 25  # ESO specifies names can be up to 25 chars long
+
 
 class User(Base):
     __tablename__ = 'users'
 
-    name = Column(String, primary_key=True)
-    user_sales = relationship("Sale")
+    name = Column(String(name_length_limit), primary_key=True)
+    sales = relationship("Sale")
+    wins = relationship("Winner")
 
     @validates('name')
     def validate_name(self, key, name_):
@@ -25,45 +28,59 @@ class User(Base):
         return name_
 
     def __repr__(self):
-        return f"<User(name={self.name}, user_sales={self.user_sales})>"
+        n_sales = len(self.sales) if self.sales is not None else 0
+        n_wins = len(self.wins) if self.wins is not None else 0
+        return f"<User(name={self.name}, {n_sales} sale(s), {n_wins} win(s))>"
 
 
 class Drawing(Base):
     __tablename__ = 'drawings'
     id = Column(Integer, Sequence('drawing_id_seq'), primary_key=True)
-    date_drawn = Column(Date, nullable=True, default=None)
+    date_started = Column(DateTime(timezone=True), nullable=False, server_default=func.now(pytz.utc))
+    date_drawn = Column(DateTime(timezone=True), nullable=True, default=None)
     sales = relationship("Sale")
     winners = relationship("Winner")
 
     def __repr__(self):
-        return f"<Drawing(id={self.id}, date_drawn={self.date_drawn}, sales={self.sales}, winners={self.winners}"
+        n_sales = len(self.sales) if self.sales is not None else 0
+        n_winners = len(self.winners) if self.winners is not None else 0
+        return f"<Drawing(id={self.id}, date_started={self.date_started}, date_drawn={self.date_drawn}, " \
+               f"{n_sales} sale(s), {n_winners} winner(s)"
+
+    @reconstructor
+    def init_on_load(self):
+        self.date_started = self.date_started.replace(tzinfo=pytz.utc).astimezone(tzlocal.get_localzone())
+        if self.date_drawn is not None:
+            self.date_drawn = self.date_drawn.replace(tzinfo=pytz.utc).astimezone(tzlocal.get_localzone())
 
 
 class Winner(Base):
     __tablename__ = 'winners'
     id = Column(Integer, Sequence('winner_id_seq'), primary_key=True)
-    drawing_id = Column(Date, ForeignKey('drawings.id'), nullable=False)
-    user_name = Column(String, ForeignKey('users.name'), nullable=False)
+    drawing_id = Column(Integer, ForeignKey('drawings.id'), nullable=False)
+    user_name = Column(String(name_length_limit), ForeignKey('users.name'), nullable=False)
     rank = Column(Integer, nullable=False)
     winnings = Column(Integer, nullable=False)
-    dwg = relationship("Drawing", back_populates="winners")
+    drawing = relationship("Drawing", back_populates="winners")
+    user = relationship("User", back_populates="wins")
 
     def __repr__(self):
-        return f"<Winner(id={self.id}, date_drawn={self.date_drawn}, user_name={self.user_name}, winnings={self.winnings}"
+        return f"<Winner(id={self.id}, drawing_id={self.drawing_id}, user_name={self.user_name}," \
+               f" rank={self.rank}, winnings={self.winnings}"
 
 
 class Sale(Base):
     __tablename__ = 'sales'
 
     id = Column(Integer, Sequence('sale_id_seq'), primary_key=True)
-    user_name = Column(String, ForeignKey('users.name'))
+    user_name = Column(String(name_length_limit), ForeignKey('users.name'), nullable=False)
     num_tickets = Column(Integer, nullable=False)
     prize_addition = Column(Boolean, nullable=False, default=False)
     time_created = Column(DateTime(timezone=True), server_default=func.now(pytz.utc))
     drawing_id = Column(Integer, ForeignKey('drawings.id'), nullable=False)
 
     drawing = relationship("Drawing", back_populates="sales")
-    user = relationship("User", back_populates="user_sales")
+    user = relationship("User", back_populates="sales")
 
     @reconstructor
     def init_on_load(self):
